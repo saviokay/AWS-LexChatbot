3a7058e1b1bdafb52a57ec8f2aa5d70e8e4fd808
saviokay <saviosebastian08@gmail.com>
2018-11-03 23:27:30 -0400

    Add Helpers Functions Like Elicit_Slot & Confirm_Intent As AWS Response Builders

--- lambda.py
+++ lambda.py
@@ -2,3 +2,33 @@ import os
 import logging
 import json
 import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# -- Helpers For Response Builders --
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return{
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def confirm_intent(session_attributes, intent_name, slots, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ConfirmIntent',
+            'intentName': intent_name,
+            'slots': slots,
+            'message': message
+        }
+    }
